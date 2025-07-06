38a0e1789a02cb5331f7e375ccc7fb7f677f19a6
Benas Kiburtas <benas.kiburtas@protonmail.com>
2025-07-07 00:07:39 +0300

    Add cryptomator package

--- home/packages/security.nix
+++ home/packages/security.nix
@@ -4,6 +4,7 @@
 {
   home.packages = with pkgs; [
     bitwarden # Password manager (GUI)
+    cryptomator # Client-side encryption for cloud storage (GUI)
     cryptsetup # Disk encryption, command: cryptsetup
     gnupg # OpenPGP implementation, command: gpg
     openssl # Cryptographic library for SSL/TLS, command: openssl
