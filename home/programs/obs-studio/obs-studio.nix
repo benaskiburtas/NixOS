bf54e855cdb41f1f82f792386391f8a0de7870e8
eveloop <26272940+eveloop@users.noreply.github.com>
2025-07-07 00:06:18 +0300

    Simplify OBS Studio configuration

--- home/programs/obs-studio/obs-studio.nix
+++ home/programs/obs-studio/obs-studio.nix
@@ -1,14 +1,29 @@
-# Open Broadcaster Software
+# Open Broadcast Software Studio
 { pkgs, ... }:
 
 {
+  # Install base package
   home.packages = with pkgs; [
     obs-studio
   ];
 
-  # Include bundled configs
-  xdg.configFile."obs-studio" = {
-    source = ./config;
-    recursive = true;
+  # Auto-start service (on login)
+  systemd.user.services.obs-studio = {
+    Unit = {
+      Description = "OBS Studio";
+      After = [ "graphical-session.target" ];
+      PartOf = [ "graphical-session.target" ];
+    };
+
+    Service = {
+      Type = "simple";
+      ExecStart = "${pkgs.obs-studio}/bin/obs --minimize-to-tray --startreplaybuffer";
+      Restart = "on-failure";
+      RestartSec = 5;
+    };
+
+    Install = {
+      WantedBy = [ "graphical-session.target" ];
+    };
   };
 }
