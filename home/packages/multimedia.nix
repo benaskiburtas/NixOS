c4ef0c792193e2d5fe07569b13b3a0c7d3a56b88
eveloop <26272940+eveloop@users.noreply.github.com>
2025-07-09 01:01:30 +0300

    Add Kdenlive video editor

--- home/packages/multimedia.nix
+++ home/packages/multimedia.nix
@@ -7,15 +7,16 @@
     discord # Chat client (GUI)
     easyeffects # Audio effects for PipeWire (GUI)
     ffmpeg # Media processing, commands: ffmpeg/ffprobe
+    kdePackages.kdenlive # Video Editor (GUI)
     kdePackages.kalk # KDE Calculator (GUI)
     krita # Digital painting (GUI)
     librewolf # Privacy-focused Firefox fork (GUI)
     mpv # Media player, command: mpv
     obsidian # Note-taking (GUI)
+    qbittorrent # Bittorrent client (GUI)
     rofi # Application launcher (GUI)
     spotify # Music streaming (GUI)
     vlc # Media player (GUI)
     wiki-tui # Wikipedia TUI client, command: wiki-tui
-    qbittorrent # Bittorrent client (GUI)
   ];
 }
