627f05cbaedc9e4dd7cee50d00e7e6defa659c33
eveloop <26272940+eveloop@users.noreply.github.com>
2025-07-16 21:36:43 +0300

    Make fish shell & kitty slightly more pretty

--- home/programs/terminal/fish.nix
+++ home/programs/terminal/fish.nix
@@ -85,6 +85,18 @@
 
       # Enable vi mode
       fish_vi_key_bindings
+
+      # ASDF configuration code
+      if test -z $ASDF_DATA_DIR
+          set _asdf_shims "$HOME/.asdf/shims"
+      else
+          set _asdf_shims "$ASDF_DATA_DIR/shims"
+      end
+
+      if not contains $_asdf_shims $PATH
+          set -gx --prepend PATH $_asdf_shims
+      end
+      set --erase _asdf_shims
     '';
 
     # Custom Fish functions
