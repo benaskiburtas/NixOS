ef6bcf72c7992dea547c429bca48ba678d1d1701
Benas Kiburtas <benas.kiburtas@protonmail.com>
2025-07-16 23:57:28 +0300

    Update ncspot configuration

--- home/programs/ncspot.nix
+++ home/programs/ncspot.nix
@@ -9,34 +9,30 @@
   # Default ncspot configuration
   xdg.configFile."ncspot/config.toml".text = ''
     [audio]
-    # Volume normalization
     volnorm = true
-    # Gapless playback
     gapless = true
-    # Set bitrate to 320kbps
     bitrate = 320
 
     [ui]
-    # Use Nerd Font icons
     use_nerdfont = true
 
     [theme]
-    background = "black"
-    primary = "light white"
-    secondary = "light black"
-    title = "green"
-    playing = "green"
-    playing_selected = "light green"
-    playing_bg = "black"
-    highlight = "light white"
-    highlight_bg = "#484848"
-    error = "light white"
-    error_bg = "red"
-    statusbar = "black"
-    statusbar_progress = "green"
-    statusbar_bg = "green"
-    cmdline = "light white"
-    cmdline_bg = "black"
-    search_match = "light red"
+    background = "#1e1e2e"          # Catppuccin Mocha Base
+    primary = "#cdd6f4"             # Catppuccin Mocha Text
+    secondary = "#6c7086"           # Catppuccin Mocha Overlay1
+    title = "#a6e3a1"               # Catppuccin Mocha Green
+    playing = "#a6e3a1"             # Catppuccin Mocha Green
+    playing_selected = "#94e2d5"    # Catppuccin Mocha Teal
+    playing_bg = "#1e1e2e"          # Catppuccin Mocha Base
+    highlight = "#b4befe"           # Catppuccin Mocha Lavender
+    highlight_bg = "#313244"        # Catppuccin Mocha Surface0
+    error = "#eba0ac"               # Catppuccin Mocha Maroon
+    error_bg = "#1e1e2e"            # Catppuccin Mocha Base
+    statusbar = "#a6e3a1"           # Catppuccin Mocha Green
+    statusbar_progress = "#b4befe"  # Catppuccin Mocha Lavender
+    statusbar_bg = "#313244"        # Catppuccin Mocha Surface0
+    cmdline = "#cdd6f4"             # Catppuccin Mocha Text
+    cmdline_bg = "#1e1e2e"          # Catppuccin Mocha Base
+    search_match = "#f9e2af"        # Catppuccin Mocha Yellow
   '';
 }
