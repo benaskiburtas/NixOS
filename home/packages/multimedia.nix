cf72b66b37d2da09f56bfbe6c3af08550d7067d8
eveloop <26272940+eveloop@users.noreply.github.com>
2025-08-16 15:09:55 +0300

    Add ani-cli, calibre; remove krita, rofi, wiki-tui packages

--- home/packages/multimedia.nix
+++ home/packages/multimedia.nix
@@ -3,21 +3,20 @@
 
 {
   home.packages = with pkgs; [
+    ani-cli # Browse and play anime, command: ani-cli
+    calibre # E-Book management software (GUI)
     cava # Audio visualizer, command: cava
     discord # Chat client (GUI)
     easyeffects # Audio effects for PipeWire (GUI)
     ffmpeg # Media processing, commands: ffmpeg/ffprobe
     kdePackages.kalk # KDE Calculator (GUI)
     kdePackages.kdenlive # Video Editor (GUI)
-    krita # Digital painting (GUI)
     librewolf # Privacy-focused Firefox fork (GUI)
     mpv # Media player, command: mpv
     obsidian # Note-taking (GUI)
     planify # Task manager with Todoist support (GUI)
     qbittorrent # Bittorrent client (GUI)
-    rofi # Application launcher (GUI)
     spotify # Music streaming (GUI)
     vlc # Media player (GUI)
-    wiki-tui # Wikipedia TUI client, command: wiki-tui
   ];
 }
